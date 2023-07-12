b779492d12666d13525e60d6e49d8f90f5384d32
fahad756 <fahadwaseem1999@gmail.com>
2023-07-12 16:21:45 +0500

    Remove duplicate 100_Days_Python_Journey folder

--- 100_Days_Python_Journey/Day 2/Excercise 1.py
+++
@@ -1,9 +0,0 @@
-#Create a calculator of performing addition, subtraction, division and multiplication
-a = 15
-b = 6
-
-
-print("The value of", a,"*",b , "is",a*b)
-print("The value of", a,"+",b , "is",a+b)
-print("The value of", a,"-",b ,"is",a-b)
-print("The value of", a,"/",b , "is",a/b)
